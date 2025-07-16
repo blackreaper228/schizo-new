96e533a5f941b2ecc96befd519f2c66b6743a091
blackreaper228 <ntsvetnikov@gmail.com>
2025-07-16 18:45:53 +0300

    Updates

---
+++ assets/articles-bitie-Di_VNxis.js
@@ -0,0 +1,1 @@
+import{m as o}from"./initReact-BEMqzw9h.js";import{a,l as e,g as t}from"./LandingGoUp-D_fwZg6E.js";import"./predictions-BuAVexAj.js";o();document.addEventListener("DOMContentLoaded",()=>{a(),e(),t()});
