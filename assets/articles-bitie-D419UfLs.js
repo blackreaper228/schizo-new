d9541cdad45cc7f9f88339c6fedbb8be7c2505ee
blackreaper228 <ntsvetnikov@gmail.com>
2025-07-16 19:02:41 +0300

    Updates

---
+++ assets/articles-bitie-D419UfLs.js
@@ -0,0 +1,1 @@
+import{m as o}from"./initReact-CDHfVsTe.js";import{a,l as e,g as t}from"./LandingGoUp-hHLiy0c5.js";import"./predictions-9KYDE934.js";o();document.addEventListener("DOMContentLoaded",()=>{a(),e(),t()});
---
+++ assets/main-BrMxNysE.js
@@ -0,0 +1,1 @@
+import{m as o}from"./initReact-CDHfVsTe.js";import{a,l as e,g as t}from"./LandingGoUp-hHLiy0c5.js";o();document.addEventListener("DOMContentLoaded",()=>{a(),e(),t()});
